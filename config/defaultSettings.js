8e6a5317a95d82e2e936b40567d907441ba0ff09
vapidity9 <lmj9lmj@163.com>
2022-07-05 18:48:53 +0800

    增加build文件

--- config/defaultSettings.js
+++ config/defaultSettings.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-    version: '1.1.1',
+    version: '1.0.0',
 };
--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -1,10 +1,22 @@
-import UseWindowSize from '@/utils/uHook'
+import { useState } from 'react';
 import styles from './index.module.less'
 
-export default function Resume () {
-  const { width, height } = UseWindowSize()
-  console.log(width, height);
+export default function Resume() {
+  const [txt, settxt] = useState(1);
+
+  //函数
+  const handle = () => {
+    settxt(txt => txt + 1);
+    setTimeout(() => {
+      console.log(txt);  //1
+      settxt(txt => {
+        console.log(txt); //2
+        return txt;
+      })
+    }, 1000)
+  };
   return <div className={styles.main}>
-    <header></header>
+    <button style={{ width: "100px", height: '100px', color: "#f00" }} onClick={handle}>点击我</button>
+    <div>{txt}</div>
   </div>
 }
